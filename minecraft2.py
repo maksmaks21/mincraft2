4f7309a3a265568dd88c78ecb06c6bbf070e9627
maksmaks21 <maksroks777@gmail.com>
2025-01-30 20:31:34 +0200

    mincraft2

--- minecraft2.py
+++ minecraft2.py
@@ -1,12 +1,14 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+from models import *
 
 app = Ursina()
 
+
 sky=Sky(texture='sky_sunset')
-ground=Entity(model='plane',scale=100 , texture='grass',texture_scale=(4,4),position=(0,-3,0))
+
+map = Map()
+map.new_map()
+
 player=FirstPersonController()
-
-block = Entity(model='cube', texture='grass', position=(2,0,0))
-
 app.run()
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from ursina import*
-
+from perlin_noise import PerlinNoise
+from ursina.shaders import basic_lighting_shader
+from numpy import floor
 
 class Block(Button):
     def __init__(self, block_type, pos,**kwaegs):
@@ -12,7 +14,22 @@ class Block(Button):
             collider='box',
             origin_y=0.5,
             color=color.color(0,0,random.uniform(0.9,1)),
+            shader= basic_lighting_shader,
             **kwaegs
         )
+
+
+class Map(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(model=None, collider=None, **kwargs)
+        bedrock=Entity(model='plane',collider='box',scale=100 , texture='grass',texture_scale=(4,4),position=(0,-3,0))
+        self.blocks = {}
+        self.noise = PerlinNoise(octaves=4, seed=-329329)
+
+    def new_map(self):
+        for x in range(60):
+            for z in range(60):
+                y = floor(self.noise([x/24, z/24])*6)
+                block = Block(0,(x,y,z))
 
 
